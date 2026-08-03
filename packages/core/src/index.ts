f5bdfbd507a0d0e03055a7318661e31d1cdc0ec0
Jagadesh Varma Nadimpalli <79473081+jagguvarma15@users.noreply.github.com>
2026-08-02 22:01:04 -0400

    Export RetentionOptions from core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -12,6 +12,6 @@ export * from "./logger";
 export * from "./ops";
 export * from "./throughline";
 export { Worker } from "./engine/worker";
-export type { WorkerDeps } from "./engine/worker";
+export type { RetentionOptions, WorkerDeps } from "./engine/worker";
 export { runWorkflow } from "./engine/run";
 export type { RunDeps, RunOutcome } from "./engine/run";
