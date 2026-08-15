18f158c039207c47eece3c8afe5fea38ad1e7ad2
Jagadesh Varma Nadimpalli <79473081+jagguvarma15@users.noreply.github.com>
2026-08-15 18:09:49 -0400

    Add the testing subpath entry

---
+++ packages/core/src/subpaths/testing.ts
@@ -0,0 +1,3 @@
+// Published as "@through-line/core/testing". Requires the optional "vitest" peer
+// dependency: the conformance suites register describe/it blocks.
+export * from "@through-line/testing";
